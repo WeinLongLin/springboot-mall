5cdec6f92627295a95b3dda85755b9ec289610ff
Maxwell <rdxwalter@gmail.com>
2023-04-22 19:32:16 +0800

    商品功能 - 實作查詢商品

---
+++ src/main/java/com/wein/springbootmall/dao/ProductDao.java
@@ -0,0 +1,8 @@
+package com.wein.springbootmall.dao;
+
+import com.wein.springbootmall.model.Product;
+
+public interface ProductDao {
+
+    Product getProductById(Integer productId);
+}
---
+++ src/main/java/com/wein/springbootmall/service/ProductService.java
@@ -0,0 +1,8 @@
+package com.wein.springbootmall.service;
+
+import com.wein.springbootmall.model.Product;
+
+public interface ProductService {
+
+    Product getProductById(Integer productId);
+}
